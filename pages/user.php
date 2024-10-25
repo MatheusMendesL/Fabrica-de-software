9f101a8daec95822fb73f5c92ea6a7158dddd104
Matheus Mendes <matheusmendelopes@gmail.com>
2024-10-25 00:37:35 -0300

    25/10 00:37

---
+++ pages/user.php
@@ -0,0 +1,42 @@
+<?php
+
+session_start();
+
+use Database_class\Database;
+
+require_once('../assets/favicon/favicon.php');
+require_once('../Database/config.php');
+require_once('../Database/Database.php');
+
+
+$login = null;
+
+
+if (empty($_SESSION['user_id'])) {
+
+    $login = false;
+
+} else {
+
+    $login = true;
+
+}
+
+$id = isset($_GET['id']) ? $_GET['id'] : "";
+$id_sessao = isset($_SESSION['user_id']) ? $_SESSION['user_id'] : "";
+
+
+if (isset($_GET['id']) && $id != $id_sessao) {
+
+    echo "O id foi alterado <br>";
+    echo "Clique nesse link para login <a href='login.php'> Aqui </a> <br>";
+    echo "Clique aqui para cadastrar <a href='cadastro.php'> Aqui </a>";
+    exit();
+
+}
+
+if(!$login){
+    header("location:cadastro.php");
+}
+
+?>
