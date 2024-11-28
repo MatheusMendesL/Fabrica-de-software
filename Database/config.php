5ded93a273839ddb7ba14838f071448aa79f3bf9
Matheus Mendes <matheusmendelopes@gmail.com>
2024-11-27 23:11:41 -0300

    FIM

--- Database/config.php
+++ Database/config.php
@@ -11,6 +11,7 @@ define('MYSQL_CONFIG', [
 
 // para o php Mailer
 
+
 define('Mailer_Host' ,'smtp.gmail.com');
 define('Mailer_Username', 'consolezone76@gmail.com');
 define('Mailer_pass', 'vofv nllg ylpf ynwy');
